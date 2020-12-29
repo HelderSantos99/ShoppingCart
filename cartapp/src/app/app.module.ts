b43651cb8af1f7118a82c66d27cc791ea9d487c6
Hélder Santos <heldersantos1999@gmail.com>
2020-12-29 19:54:18 +0000

    Interface pronta (filtros, produtos e carrinho)
    - Conteúdo estático

--- cartapp/src/app/app.module.ts
+++ cartapp/src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { ShoppingcartComponent } from './components/shoppingcart/shoppingcart.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { FiltersComponent } from './components/shoppingcart/filters/filters.component';
+import { ProductListComponent } from './components/shoppingcart/product-list/product-list.component';
+import { CartComponent } from './components/shoppingcart/cart/cart.component';
+import { CartItemComponent } from './components/shoppingcart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './components/shoppingcart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    ShoppingcartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
