e267b89f9416b4331e6503b151f6139b23708044
Hélder Santos <heldersantos1999@gmail.com>
2021-01-10 00:55:51 +0000

    API de Produtos

--- cartapp/src/app/services/product.service.ts
+++ cartapp/src/app/services/product.service.ts
@@ -1,23 +1,18 @@
+import { Observable } from 'rxjs';
 import { Product } from './../models/product';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const apiUrl = 'http://localhost:3000/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  products: Product[] = [
-    new Product (1, 'Product 1', 'Iphone XR v1', 100),
-    new Product (2, 'Product 2', 'Iphone XR v2', 200),
-    new Product (3, 'Product 3', 'Iphone XR v3', 300),
-    new Product (4, 'Product 4', 'Iphone XR v4', 400),
-    new Product (5, 'Product 5', 'Iphone XR v5', 500),
-    new Product (6, 'Product 6', 'Iphone XR v6', 600)
-  ];
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  getProducts(): Product[] {
-    return this.products;
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(apiUrl);
   }
 }
